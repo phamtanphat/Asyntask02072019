bd9c560092bf66a613e7c99dfbd2e4705a6d0a32
phatdroid94 <phatdroid94@gmail.com>
2019-09-13 18:29:15 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
@@ -2,6 +2,7 @@ package phamtanphat.ptp.khoaphamtraining.asyntask02072019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,61 +17,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txt = findViewById(R.id.textview);
-
+        //class vo danh
+        xulytientrinh().execute();
     }
     //Nested class
     // inner class : Delegate
-
     // params : gia tri truyen vao cho phan xu ly logic
     // progress : gia tri khi xu ly se cap nhat len cho view
     // result : sau khi thuc thi xog day la du lieu tra ve
-
-
     //Bat dau thuc thi
     //  Thuc thi cong viec 1
     //  Thuc thi cong viec 2
     //  Thuc thi cong viec 3
     //Thuc thi ket thuc
     //Ket thuc phan xu ly
-    class Xulytientrinh extends AsyncTask<Void,String,String> {
-        @Override
-        protected void onPreExecute() {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    txt.setText("Bat dau thuc thi \n");
-                }
-            }, 2000);
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            for (int i = 1 ; i <= 3 ; i++ ){
-                try {
-                    String congviec = "Thuc thi cong viec " + i;
-                    Thread.sleep(1000);
-                    publishProgress(congviec);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    private AsyncTask<Void , String ,String> xulytientrinh(){
+        return new AsyncTask<Void, String, String>() {
+            @Override
+            protected void onPreExecute() {
+                txt.setText("Bat dau thuc thi \n");
             }
-            return "Thuc thi ket thuc";
-        }
-        @Override
-        protected void onProgressUpdate(String... values) {
-            txt.append(values[0] + "\n");
-            super.onProgressUpdate(values);
-        }
 
-        @Override
-        protected void onPostExecute(String s) {
-            txt.append(s + "\n");
-            txt.append("Ket thuc phan xu ly");
-            super.onPostExecute(s);
-        }
+            @Override
+            protected String doInBackground(Void... voids) {
+                for (int i = 1 ; i <= 3 ; i++ ){
+                    try {
+                        String congviec = "Thuc thi cong viec " + i;
+                        Thread.sleep(1000);
+                        publishProgress(congviec);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return "Thuc thi ket thuc";
+            }
+            @Override
+            protected void onProgressUpdate(String... values) {
+                txt.append(values[0] + "\n");
+                super.onProgressUpdate(values);
+            }
 
+            @Override
+            protected void onPostExecute(String s) {
+                txt.append(s + "\n");
+                txt.append("Ket thuc phan xu ly");
+                super.onPostExecute(s);
+            }
+        };
     }
+
 
 
 }
