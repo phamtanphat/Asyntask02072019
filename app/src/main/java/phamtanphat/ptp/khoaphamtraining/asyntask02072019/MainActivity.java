ee9b01001ad0fdce11be5587d80637cbcc6e662d
phatdroid94 <phatdroid94@gmail.com>
2019-09-14 18:26:28 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String ten;
+    TextView mtxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mtxt = findViewById(R.id.textview);
         //Bat dau thuc thi
         //  Thuc thi cong viec 1
         //  Thuc thi cong viec 2
@@ -30,21 +32,39 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+            mtxt.setText("Bat dau thuc thi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             super.onPreExecute();
         }
 
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            for (int i = 1 ; i <= 3 ; i++){
+                String congviec = "Thuc thi cong viec " + i;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(congviec);
+            }
+            return "Thuc thi ket thuc";
         }
 
         @Override
         protected void onProgressUpdate(String... values) {
+            mtxt.append("\n" + values[0]);
             super.onProgressUpdate(values);
         }
 
         @Override
         protected void onPostExecute(String s) {
+            mtxt.append("\n" + s);
+            mtxt.append("\n" + "Ket thuc phan xu ly");
             super.onPostExecute(s);
         }
     }
