712cfacdfe941abbae29175e8b6df8928a05e32b
phatdroid94 <phatdroid94@gmail.com>
2019-09-14 19:29:19 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/asyntask02072019/MainActivity.java
@@ -2,8 +2,10 @@ package phamtanphat.ptp.khoaphamtraining.asyntask02072019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,48 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         // class vo danh
-        new Xulytientrinh().execute();
+        xuytientrinh().execute();
     }
-    //Nested class
-    // inner class : Delegate
-
-    // params : gia tri truyen vao cho phan xu ly logic
-    // progress : gia tri khi xu ly se cap nhat len cho view
-    // result : sau khi thuc thi xog day la du lieu tra ve
-    class Xulytientrinh extends AsyncTask<String, String, String> {
-
-        @Override
-        protected void onPreExecute() {
-            mtxt.setText("Bat dau thuc thi");
-            this.cancel(true);
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            for (int i = 1; i <= 3; i++) {
-                String congviec = "Thuc thi cong viec " + i;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                publishProgress(congviec);
+    @SuppressLint("StaticFieldLeak")
+    private AsyncTask<String,String,String> xuytientrinh(){
+        return new AsyncTask<String, String, String>() {
+            @Override
+            protected String doInBackground(String... strings) {
+                return null;
             }
-            return "Thuc thi ket thuc";
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            mtxt.append("\n" + values[0]);
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            mtxt.append("\n" + s);
-            mtxt.append("\n" + "Ket thuc phan xu ly");
-            super.onPostExecute(s);
-        }
+        };
     }
+
 }
